e2d396b9524e92f8a18a0af37a3adbde7efe3f1c
Prasad Reddy <prasad.reddy342@gmail.com>
2017-09-15 15:26:37 +0530

    css and js topics

---
+++ JS/expjsonobj.js
@@ -0,0 +1,203 @@
+// var personDetails = {
+//     "title": "Person",
+//     "type": "object",
+//     "properties": {
+//         "firstName": { "type": "string" },
+//         "lastName": { "type": "string" },
+//         "age": {
+//             "description": "Age in years",
+//             "type": "integer",
+//             "minimum": 0
+//         }
+//     },
+//     "required": ["fname", "lname"]
+// }
+
+
+// for (details in personDetails) {
+//     console.log("keys=", details);
+//     console.log("values=", personDetails[details]);
+//     console.log("data type=", typeof personDetails[details]);
+//     var propdetail = personDetails[details];
+
+//     if (typeof propdetail === 'object') {
+
+//         for (property in propdetail) {
+
+//             console.log("keys=", property);
+//             console.log("values=", propdetail[property]);
+//             console.log("data type=", typeof propdetail[property]);
+//             //console.log(propdetail[property]);
+
+//             var insproperty = propdetail[property]
+//             if (typeof insproperty === 'object') {
+//                 for (insprop in insproperty) {
+//                     console.log("keys=", insprop);
+//                     console.log("values=", insproperty[insprop]);
+//                     console.log("datatype=", typeof insproperty[insprop]);
+
+//                 }
+
+//             }
+
+//         }
+
+//         for (var i = 0; i < propdetail.length; i++) {
+//             console.log(propdetail[i]);
+
+//         }
+//     }
+
+// }
+
+// ----------------------Another way--------------------
+
+// for (details in personDetails) {
+//     console.log("keys=", details, "values=", personDetails[details], "data type=", typeof personDetails[details]);
+
+//     var propdetail = personDetails[details];
+
+//     if (typeof propdetail === 'object') {
+
+//         for (property in propdetail) {
+
+//             console.log("keys=", property, "values=", propdetail[property], "data type=", typeof propdetail[property]);
+
+
+//             var insproperty = propdetail[property]
+//             if (typeof insproperty === 'object') {
+//                 for (insprop in insproperty) {
+//                     console.log("keys=", insprop, "values=", insproperty[insprop], "datatype=", typeof insproperty[insprop]);
+//                 }
+
+//             }
+
+//         }
+
+//         for (var i = 0; i < propdetail.length; i++) {
+//             console.log(propdetail[i]);
+
+//         }
+//     }
+
+// }
+
+//---------Another Example-------------------------------------------------------------
+
+// var obj = {
+//     "menu": {
+//         "id": "file",
+//         "value": "File",
+//         "popup": {
+//             "menuitem": [
+//                 { "value": "New", "onclick": "CreateNewDoc()" },
+//                 { "value": "Open", "onclick": "OpenDoc()" },
+//                 { "value": "Close", "onclick": "CloseDoc()" }
+//             ]
+//         }
+//     }
+// }
+
+
+
+// for (obj1 in obj) {
+//     console.log(obj1, obj[obj1], typeof obj[obj1]);
+//     var obj1info = obj[obj1];
+//     if (typeof obj1info === 'object')
+//         for (mobj in obj1info) {
+//             console.log(mobj, obj1info[mobj], typeof obj1info[mobj]);
+//             var mobjdata = obj1info[mobj];
+//             if (typeof mobjdata === 'object') {
+//                 for (popobj in mobjdata) {
+//                     console.log(popobj, mobjdata[popobj], typeof mobjdata[popobj]);
+//                     var popobjdata = mobjdata[popobj];
+//                     if (typeof popobjdata === 'object') {
+//                         for (var i = 0; i < popobjdata.length; i++) {
+//                             var menuitemdata = popobjdata[i];
+//                             console.log(menuitemdata);
+//                             for (menuiteminsideobj in menuitemdata) {
+//                                 console.log(menuiteminsideobj, menuitemdata[menuiteminsideobj]);
+//                             }
+//                         }
+//                     }
+
+//                 }
+//             }
+
+//         }
+
+
+// }
+
+
+//----------------------recursion function or loop in JS-------------------------------
+
+
+function func(n) {
+
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+   var result = n;
+
+    while (n > 1) {
+        
+        result = result * (n - 1);
+        n = n - 1;
+
+    }
+    return result;
+
+   // recursion process
+
+   // return n*func(n-1);
+
+
+}
+
+    document.write(func(6));
+   // console.log(func(5));
+
+//-----------------------------------------------
+
+
+function func1(n){
+
+    if(n>5){
+
+        return n;
+    }
+
+    else{
+
+        return n+func1(n-1);
+    }
+
+}
+document.write("--------------------",func1(4));
+
+//------------------------------------------------------
+
+function func(n) {
+
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+   //var result = n;
+
+    // while (n > 1) {
+        
+    //     result = result * (n - 1);
+    //     n = n - 1;
+
+    // }
+
+
+   // recursion process
+
+   return n*func(n-1);
+
+
+}
+
+    document.write(func(4));
--- JS/tempjson.js
+++ JS/tempjson.js
@@ -32,19 +32,19 @@ var empList = {
 }
 
 
-var test = {
-    "inida": ['AP', "TL", "MP"],
-    "us": ['xx', "tt", "dd"],
-    "inida": ['AP1', "TL", "MP"],
-    "inida": ['AP2', "TL", "MP"],
-    "inida": ['AP3', "TL", "MP"],
-    "inida": ['AP4', "TL", "MP"],
-    "inida": ['AP5', "TL", "MP"],
-    "inida": ['AP6', "TL", "MP"],
-    "inida": ['AP7', "TL", "MP"],
-    "inida": ['AP8', "TL", "MP"],
-    "inida": ['APt', "TL", "MP"]
-};
+// var test = {
+//     "inida": ['AP', "TL", "MP"],
+//     "us": ['xx', "tt", "dd"],
+//     "inida": ['AP1', "TL", "MP"],
+//     "inida": ['AP2', "TL", "MP"],
+//     "inida": ['AP3', "TL", "MP"],
+//     "inida": ['AP4', "TL", "MP"],
+//     "inida": ['AP5', "TL", "MP"],
+//     "inida": ['AP6', "TL", "MP"],
+//     "inida": ['AP7', "TL", "MP"],
+//     "inida": ['AP8', "TL", "MP"],
+//     "inida": ['APt', "TL", "MP"]
+// };
 
 //console.log(test['inida']);
 //console.log(test['us']);
@@ -54,24 +54,26 @@ var test = {
 
 
 
-for (myvar in empList) {
-    console.log("Keys= ", myvar, "Values :", empList[myvar], "data type: ", typeof empList[myvar])
+// for (myvar in empList) {
+//     console.log("Keys= ", myvar, "Values :", empList[myvar], "data type: ", typeof empList[myvar])
 
-    console.log(myvar)
+//     console.log(myvar)
 
-    if (typeof empList[myvar] === 'object') {
-        
-        for (var i = 0; i < empList[myvar].length; i++) {
-            var record = empList[myvar][i];
-            for (prasad in record) {
-                console.log(record[prasad])
-            }
-        }
-       
-    }
+//     if (typeof empList[myvar] === 'object') {
 
-}
+//         for (var i = 0; i < empList[myvar].length; i++) {
+//             var record = empList[myvar][i];
+//             for (prasad in record) {
+//                 console.log(record[prasad])
+//             }
+//         }
 
+//     }
+
+// }
+
+
+//----------------------------------------------------
 
 // for(obj in empList){
 //     console.log("Keys=", obj, "Value:", empList[obj], "data type:", typeof empList[obj]);
@@ -81,23 +83,77 @@ for (myvar in empList) {
 
 
 
-// ---------------below "objStr" is a string -----------------
+// ---------------below "objStr" is a string -------------------------------------------------------
 
 
-var objStr = '{"id": 323232,"Name": "xxxxxxx","DOB": "new Date()","salary": 34343.51,"isIndian": true,"prevEmp": "null"}';
-console.log(" string ",objStr);
+// var objStr = '{"id": 323232,"Name": "xxxxxxx","DOB": "new Date()","salary": 34343.51,"isIndian": true,"prevEmp": "null"}';
+// console.log(" string ", objStr);
 
 // ------------convert from string to object----------------
 
-var convertObj = JSON.parse(objStr);
-console.log(" object= ",convertObj, " id =",convertObj['id']);
+// var convertObj = JSON.parse(objStr);
+// console.log(" object= ", convertObj, " id =", convertObj['id']);
 
 
 //-------------- name changed-----------------
-console.log("Name=", convertObj['Name'] = "ravindra");
+// console.log("Name=", convertObj['Name'] = "ravindra");
 
 //------------- convert from object to string
 
-var convertStr= JSON.stringify(convertObj);
+// var convertStr = JSON.stringify(convertObj);
 
-console.log(" convert to String ",convertStr);
+// console.log(" convert to String ", convertStr);
+
+
+//----------parseInt and Float---------------------
+
+// var a = parseInt("10");
+// var b = parseInt("10.25");
+// var c = parseInt("10 years");
+// var d = parseInt("years 10");
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+// var a = parseFloat("10");
+// var b = parseFloat("10.25");
+// var c = parseFloat("10 years");
+// var d = parseFloat("years 10");
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+// var a = isNaN(10);
+// var b = isNaN("10.25");
+// var c = isNaN("10 years");
+// var d = isNaN("years 10");
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+//---calculate the evaluate/expression code--------
+// var x = 10;
+// y = 20;
+// var a = eval("10+20");
+// var b = eval("10.25+20.50");
+// var c = eval("10*2");
+// var d = eval("x+y");
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+//----repeated character findout in a string---------
+
+// var word = "bannanas";
+// var guess = "n";
+// console.log(word.indexOf(guess));
+
+// var str = "scissors";
+// var indices = [];
+// for (var i = 0; i < str.length; i++) {
+//     if (str[i] == "s")
+//         console.log(str[i]);
+//console.log(indices.push(i));
